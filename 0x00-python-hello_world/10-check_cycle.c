999effc4609d63f58d411da9352e3824330d3342
Kingsley Okpo <kngsley2018@gmail.com>
2024-01-03 03:45:16 +0100

    100-write.py

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,35 @@
+#include "lists.h"
+
+/**
+ * check_cycle - This finds the loop in a linked list
+ * @list: linked list to search for
+ * Return: The address of the node where the loop starts, or NULL
+ */
+
+
+int check_cycle(listint_t *list)
+{
+		int i;
+		listint_t *slow = list;
+		listint_t *quick = list;
+
+		if (!list)
+				return (0);
+		while (slow && quick && quick->next)
+		{
+				quick = quick->next->next;
+				slow = slow->next;
+				if (quick == slow)
+				{
+						slow = list;
+						while (slow != quick)
+						{
+								slow = slow->next;
+								quick = quick->next;
+						}
+						return (i);
+				}
+				i++;
+		}
+		return (0);
+}
