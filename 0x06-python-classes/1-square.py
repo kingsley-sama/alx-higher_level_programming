e0ae50806de0d22da8a68e53671efe9d1c84cae8
Kingsley Okpo <kngsley2018@gmail.com>
2024-01-24 05:26:34 +0100

    creates: files

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+"""Defines a class Square"""
+
+
+class Square:
+    """This represents a square"""
+
+    def __init__(self, size):
+        """This initialize a new Square
+
+        Args:
+            size (int): The size of the new square
+        """
+        self.__size = size
