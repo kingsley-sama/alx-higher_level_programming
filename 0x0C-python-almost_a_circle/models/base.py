b703422350dba947349d42b47d0300e7bec97e98
Kingsley Okpo <kngsley2018@gmail.com>
2024-03-01 18:42:14 +0100

    fixes base.py

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -5,11 +5,10 @@
 class Base:
     """defines the Base class"""
 
-
-def __init__(self, id=0):
-    self.__nb_objects = 0
-    if id is not None:
-        self.id = id
-    else:
-        self.__nb_objects += 1
-        self.id = __nb_objects
+    def __init__(self, id=0):
+        self.__nb_objects = 0
+        if id is not None:
+            self.id = id
+        else:
+            self.__nb_objects += 1
+            self.id = __nb_objects
