e781cace5a78edc367e870788b5c1142aadbe1e4
Kingsley Okpo <kngsley2018@gmail.com>
2024-04-09 00:38:13 +0100

    updates 103-object_fct.js

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -1,10 +1,12 @@
 #!/usr/bin/node
+function incr () { this.value++; }
 const myObject = {
   type: 'object',
-  value: 12,
-  incr: () => this.value++
+  value: 12
+
 };
 console.log(myObject);
+myObject.incr = incr;
 myObject.incr();
 console.log(myObject);
 myObject.incr();
