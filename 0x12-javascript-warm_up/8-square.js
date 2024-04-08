b1f739530269aa2d8e611c9af3fdf6b7e1b9a24c
Kingsley Okpo <kngsley2018@gmail.com>
2024-04-09 00:00:07 +0100

    updats 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,7 +1,5 @@
-#!/usr/bin/env node
-const { link } = require('node:fs');
-const { argv } = require('node:process');
-const myStr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+#!/usr/bin/node
+const { argv } = require('process');
 for (let i = 0; i < parseInt(argv[2]); i++) {
   let line = '';
   for (let k = 0; k < parseInt(argv[2]); k++) {
