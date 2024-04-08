d0fd5e13313f42e105ef6127776377693d94d6e5
Kingsley Okpo <kngsley2018@gmail.com>
2024-04-09 00:10:37 +0100

    updats 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 const { argv } = require('process');
-if (parseInt(argv[0])) {
+if (parseInt(argv[2])) {
   for (let i = 0; i < parseInt(argv[2]); i++) {
     let line = '';
     for (let k = 0; k < parseInt(argv[2]); k++) {
