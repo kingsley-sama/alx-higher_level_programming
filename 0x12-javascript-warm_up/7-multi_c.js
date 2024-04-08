8dcc23159ae5f7ae2c9fa0b7c7d1a2ca35d863b0
Kingsley Okpo <kngsley2018@gmail.com>
2024-04-08 23:58:18 +0100

    updats 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,5 @@
 #!/usr/bin/env node
 const { argv } = require('node:process');
-const myStr = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
 for (let i = 0; i < parseInt(argv[2]); i++) {
   console.log('C is fun');
 }
