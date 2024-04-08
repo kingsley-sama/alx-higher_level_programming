8d4ce4073df2cf90133e4b19fe3015a400dc748f
Kingsley Okpo <kngsley2018@gmail.com>
2024-04-09 00:09:31 +0100

    updats 8-square.js

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,9 +1,13 @@
 #!/usr/bin/node
 const { argv } = require('process');
-for (let i = 0; i < parseInt(argv[2]); i++) {
-  let line = '';
-  for (let k = 0; k < parseInt(argv[2]); k++) {
-    line += 'X';
+if (parseInt(argv[0])) {
+  for (let i = 0; i < parseInt(argv[2]); i++) {
+    let line = '';
+    for (let k = 0; k < parseInt(argv[2]); k++) {
+      line += 'X';
+    }
+    console.log(line);
   }
-  console.log(line);
+} else {
+  console.log('Missing Size');
 }
