78e85b9643598a2da23d348bb5c28b41c3085b4b
Kingsley Okpo <kngsley2018@gmail.com>
2024-04-08 23:42:35 +0100

    updates 1-multi_languages.js

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-const myStr = ['C is fun', 'Python is cfool', 'JavaScript is amazing'];
-for (let i = 0; i < myStr.length; i++) {
-  console.log(myStr[i]);
-}
+console.log('C is fun');
+console.log('Python is cfool');
+console.log('JavaScript is amazing');
