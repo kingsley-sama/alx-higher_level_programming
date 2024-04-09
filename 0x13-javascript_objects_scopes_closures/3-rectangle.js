e026289b050b8856a461b39468b78ad8ffb276d7
Kingsley Okpo <kngsley2018@gmail.com>
2024-04-09 17:24:39 +0100

    updates 3-rectangle.js

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,19 +1,18 @@
 #!/usr/bin/node
 class Rectangle {
-  constructor(w, h) {
+  constructor (w, h) {
     if (h > 0 && w > 0) {
       this.height = h;
       this.width = w;
-    }
-    else {
+    } else {
       return {};
     }
   }
-  print() {
+
+  print () {
     for (let i = 0; i < this.height; i++) {
-       console.log(('X').repeat(this.width));
+      console.log(('X').repeat(this.width));
     }
   }
-
 }
 module.exports = Rectangle;
