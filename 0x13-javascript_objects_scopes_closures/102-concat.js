d2613ca81797257e1751e682b334500596e4a557
Kingsley Okpo <kngsley2018@gmail.com>
2024-04-10 22:23:08 +0100

    102-concat.js

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,20 +1,22 @@
 #!/usr/bin/node
-const { argv } = require('process');
 const fs = require('fs');
 
-const a = argv[2];
-const b = argv[3];
-const c = argv[4];
+const fileA = process.argv[2];
+const fileB = process.argv[3];
+const fileC = process.argv[4];
 
-if (a && b && c) {
-  concat(a, b);
-}
+if (
+  fs.existsSync(fileA) &&
+fs.statSync(fileA).isFile &&
+fs.existsSync(fileB) &&
+fs.statSync(fileB).isFile &&
+fileC !== undefined
+) {
+  const fileAContent = fs.readFileSync(fileA);
+  const fileBContent = fs.readFileSync(fileB);
+  const stream = fs.createWriteStream(fileC);
 
-function concat (a, b) {
-  try {
-    const fileA = fs.readFileSync(a, 'utf-8');
-    const fileB = fs.readFileSync(b, 'utf-8');
-    fs.writeFileSync(c, fileA + '\n' + fileB);
-  } catch (err) {
-  }
+  stream.write(fileAContent);
+  stream.write(fileBContent);
+  stream.end();
 }
