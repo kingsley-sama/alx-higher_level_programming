06fc9da7ad834393e48da7491bfbd1608bd3f92f
Kingsley Okpo <kngsley2018@gmail.com>
2024-04-14 01:38:01 +0100

    4-cities_by_state.py

---
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -0,0 +1,24 @@
+#!/usr/bin/python3
+""" This module defines the connectDb function that conects to a database"""
+import MySQLdb
+import sys
+
+
+def connectDb():
+
+    """this functions makes a connection to the database"""
+    dbConnect = MySQLdb.connect(user=sys.argv[1],
+                                password=sys.argv[2],
+                                database=sys.argv[3],
+                                host="localhost",
+                                port=3306)
+    cur = dbConnect.cursor()
+    data = cur.fetchall()
+    for i in data:
+        print(i)
+    cur.close()
+    dbConnect.close()
+
+
+if __name__ == "__main__":
+    connectDb()
