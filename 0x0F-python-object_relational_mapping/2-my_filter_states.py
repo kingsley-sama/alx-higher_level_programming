1b459dbe6b75f5ae1bc1719813962abda7192329
Kingsley Okpo <kngsley2018@gmail.com>
2024-04-14 01:19:41 +0100

    2-my_filter_states.py

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" This module defines filter state that returns all states starting with N"""
+import MySQLdb
+import sys
+
+
+def findstate():
+
+    """this functions makes a connection to the database and filters state"""
+    dbConnect = MySQLdb.connect(user=sys.argv[1],
+                                password=sys.argv[2],
+                                database=sys.argv[3],
+                                host="localhost",
+                                port=3306)
+    cur = dbConnect.cursor()
+    cur.execute("SELECT * FROM states WHERE name = '{}'".format(sys.argv[4]))
+    data = cur.fetchall()
+    for i in data:
+        print(i)
+    cur.close()
+    dbConnect.close()
+
+
+if __name__ == "__main__":
+    findstate()
