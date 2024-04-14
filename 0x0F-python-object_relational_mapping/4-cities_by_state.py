6d5fb7de664e32b0d2ad7f41fd066fc51aa8bfb4
Kingsley Okpo <kngsley2018@gmail.com>
2024-04-14 22:19:40 +0100

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -13,6 +13,7 @@ def connectDb():
                                 host="localhost",
                                 port=3306)
     cur = dbConnect.cursor()
+    cur.execute("SELECT cities.id, cities.name, states.name FROM cities JOIN states ON cities.state_id = states.id ORDER BY cities.id ASC")
     data = cur.fetchall()
     for i in data:
         print(i)
