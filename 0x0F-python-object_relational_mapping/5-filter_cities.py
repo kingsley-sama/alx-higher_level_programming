06f20d407bc0c29a584da7648f6ddf5ef945c329
Kingsley Okpo <kngsley2018@gmail.com>
2024-04-15 11:52:27 +0100

    4-cities_by_state.py

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -1,28 +1,29 @@
 #!/usr/bin/python3
-# List all states with a name starting with uppercase N
-# Username, password, and database names are given as user args
+""" This module defines the connectDb function that conects to a database"""
 import MySQLdb
 import sys
 
 
-def findstate():
+def connectDb():
 
-    """this functions makes a connection to the database and filters state"""
+    """this functions makes a connection to the database"""
     dbConnect = MySQLdb.connect(user=sys.argv[1],
                                 password=sys.argv[2],
                                 database=sys.argv[3],
                                 host="localhost",
                                 port=3306)
     cur = dbConnect.cursor()
-    cur.execute("""SELECT id,
-    name FROM states WHERE name LIKE 'N%'
-    ORDER BY id ASC;""")
+    cur.execute("""SELECT states.name, cities.name FROM cities INNER JOIN states
+    ON cities.state_id = states.id
+    ORDER BY cities.id ASC;""")
     data = cur.fetchall()
     for i in data:
-        print(i)
+        if i[0] == sys.argv[4]:
+            print("{}, ".format(i[1]), end='')
+    print("")
     cur.close()
     dbConnect.close()
 
 
 if __name__ == "__main__":
-    findstate()
+    connectDb()
